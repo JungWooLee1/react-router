76e553928ec7f331134719a88db0a1e1e155d7a6
이정우 <ijeong-u@ijeong-uui-iMac.local>
2018-11-06 22:05:35 +0900

    [130,131] link 컴포넌트 사용하기

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 
@@ -24,6 +25,11 @@ class PostsIndex extends Component {
   render() {
     return (
         <div>
+          <div className="text-xs-right">
+            <Link className="btn btn-primary" to="/posts/new">
+              Add a Post
+            </Link>
+          </div>
           <h3>Posts</h3>
           <ul className="list-group">
             {this.renderPosts()}
@@ -39,4 +45,4 @@ function mapStateToProps(state) {
 }
 
 // es6 => fetchPosts: fetchPosts() => fetchPosts  (mapDispatchToProps)
-export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
+export default connect( mapStateToProps, { fetchPosts } )(PostsIndex);
