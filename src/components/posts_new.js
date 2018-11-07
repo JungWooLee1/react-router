eac4aaaae0c74b4fef5cab9f7fa1be1d6b6fe421
이정우 <ijeong-u@ijeong-uui-iMac.local>
2018-11-08 00:42:38 +0900

    [132,133,134,135] redux-form 을 이용한 input form 만들기

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,47 @@
 import React, {Component} from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+
+  renderField(field) {
+    return (
+        <div className="form-group">
+          <label>{field.label}</label>
+          <input className="form-control" type="text" {...field.input}/>
+        </div>
+    )
+  }
+
   render() {
     return (
-        <div>
-          PostsNew!
-        </div>
+        <form>
+          <Field
+              label="Title For Post"
+              name="title"
+              component={this.renderField}>
+          </Field>
+
+          <Field
+              label="Tags"
+              name="tags"
+              component={this.renderField}>
+          </Field>
+
+          <Field
+              label="Post Content"
+              name="content"
+              component={this.renderField}>
+          </Field>
+
+        </form>
     )
   }
 }
 
-export default PostsNew;
+// 구성 옵션을 적어준다.
+// form의 이름을 설정한다.
+
+export default reduxForm({
+  form : 'PostsNewForm',
+
+})(PostsNew);
