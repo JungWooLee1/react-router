876a7079ef53865af60c577db85ce980ca104133
이정우 <ijeong-u@ijeong-uui-iMac.local>
2018-11-09 01:16:37 +0900

    [136,137,138] redux-from 유효성 검사하기

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -8,13 +8,21 @@ class PostsNew extends Component {
         <div className="form-group">
           <label>{field.label}</label>
           <input className="form-control" type="text" {...field.input}/>
+          { field.meta.error }
         </div>
     )
   }
 
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+
+    const { handleSubmit } = this.props;
+
     return (
-        <form>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
           <Field
               label="Title For Post"
               name="title"
@@ -22,8 +30,8 @@ class PostsNew extends Component {
           </Field>
 
           <Field
-              label="Tags"
-              name="tags"
+              label="Categories"
+              name="categories"
               component={this.renderField}>
           </Field>
 
@@ -32,16 +40,44 @@ class PostsNew extends Component {
               name="content"
               component={this.renderField}>
           </Field>
-
+          <button type="submit" className="btn btn-primary">Submit</button>
         </form>
     )
   }
+}
+
+function validate(values) {
+  // console.log(values) -> { title: 'asdf', categories: 'asdf' content: 'asd'}
+  const errors ={};
+
+  // Validate the inputs from 'values' => values로부터 유효성을 검사한다.
+
+  if ((values.title + "").length < 3) {
+    errors.title = "Title must be at least 3 characters";
+  }
+
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content please";
+  }
+
+  // If errors is empty, the form is fine to submit
+  // If errors has *any* properties, redux from assumes form is invaild
+  return errors;
 }
 
 // 구성 옵션을 적어준다.
 // form의 이름을 설정한다.
 
 export default reduxForm({
+  validate,
   form : 'PostsNewForm',
 
 })(PostsNew);
