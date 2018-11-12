dd1b7c6a1297355a2e1dd9a7eca0c3860780eba1
이정우 <ijeong-u@ijeong-uui-iMac.local>
2018-11-13 00:23:52 +0900

    [145] Receiving New Posts

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,16 @@
-import { FETCH_POSTS } from "../actions";
 import _ from 'lodash';
+import { FETCH_POSTS, FETCH_POST } from "../actions";
+
 
 export default function(state = {}, action) {
   switch (action.type) {
+
+    case FETCH_POST :
+      //const post = action.payload.data;
+      //const newState = { ...state }; => ...은 가변인자를 말한다. ES6에서 {...state}는 0 : first, 1 : second .... 이런식으로 표현된
+      //newState[post.id] = post;
+      //return newState;
+      return { ...state, [action.payload.data.id] : action.payload.data};
     case FETCH_POSTS :
       // {id :4, title, "hi"}, {id: 5, title: "good-bye"} 등을 key를 id로 가지도록 변환 => ex) {"4" : {id: 4, title: "hi},.... }
       return _.mapKeys(action.payload.data, 'id');
