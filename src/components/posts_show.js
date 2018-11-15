bb6b823909e9bdc81bc77271eeb32210adc9c879
이정우 <ijeong-u@ijeong-uui-iMac.local>
2018-11-16 01:16:47 +0900

    [146] Selecting from OwnProps

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -9,7 +9,12 @@ class PostsShow extends Component {
     this.props.fetchPost(id);
   }
 
+  helperFunction() {
+    this.props.posts[this.props.match.params.id];
+  }
+
   render() {
+    this.props.match.params.id;
     return (
         <div>
           Posts Show!
@@ -18,16 +23,12 @@ class PostsShow extends Component {
   }
 }
 
-function mapStateToProps({ posts }) {
-  return { posts };
-}
-
 function mapStateToProps({ posts }, ownProps) {
-
+  return {post : posts[ownProps.match.parms.id]};
 }
 
 // 컴포넌트와 리덕스를 연동할 때 connect 를 사용합니다.
 // connect() 의 결과는, 컴포넌트에 props를 넣어주는 함수를 반환합니다.
 // 반환된 함수에 우리가 만든 컴포넌트를 넣어주면 됩니다.
 
-export default connect(null, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
